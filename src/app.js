a361323b39dfd0ca896879d6c5916dfe829504da
Foridul35962 <foridulislam35962@gmail.com>
2025-12-14 18:07:57 +0600

    global error added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 
+// local imports
+import errorHandler from './utils/ErrorHandler.js'
+
+
 const app = express()
 
 app.use(cors({
@@ -15,13 +19,12 @@ app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-// local routes
-
 
 app.get('/', (req, res) => {
     res.send('Food Bridge server is running...')
 })
 
-
+//global error handler
+app.use(errorHandler)
 
 export default app
